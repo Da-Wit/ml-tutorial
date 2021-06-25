cd5e4b07c8178789f68fc7e3b117aefb433e6333
dawit jeong <wjdekdnlt1215@gmail.com>
2021-06-25 09:43:57 +0900

    done with chapter 6 in fdk

---
+++ 6_data_saving/data_saving.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+def createExamples():
+    numberArrayExamples = open('6_data_saving/numArEx.txt','a')
+    numbersWeHave = range(1, 10)
+    print("numbersWeHave:",numbersWeHave)
+    for eachNum in numbersWeHave:
+        # print eachNum
+        for furtherNum in numbersWeHave:
+            print(str(eachNum) + '.' + str(furtherNum))
+            imageFilePath = 'images/numbers/' + str(eachNum) + '.' + str(furtherNum) + '.png'
+            ei = Image.open(imageFilePath)
+            eiar = np.array(ei)
+            eiarl = str(eiar.tolist())
+
+            print(eiarl)
+            lineToWrite = str(eachNum) + '::' + eiarl + '\n'
+            numberArrayExamples.write(lineToWrite)
+createExamples()
